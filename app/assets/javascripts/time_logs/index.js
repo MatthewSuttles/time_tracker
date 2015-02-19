ae67a6cad5d9fd73cbf984aeae68d0f1bb0fd661
MatthewSuttles <msuttles@accruent.com>
2015-02-19 12:43:40 -0600

    added verification

--- app/assets/javascripts/time_logs/index.js
+++ app/assets/javascripts/time_logs/index.js
@@ -3,6 +3,8 @@
  */
 
 $(document).ready(function(){
+    var teamID, subcategoryID, date, hours, minutes;
+
     $('.datepicker').datepicker();
 
 
@@ -40,33 +42,53 @@ $(document).ready(function(){
 
     //----------Submit The Time Log-----------------------
     $('body').on('click','#submit_time_log', function(e){
-        var teamID = $('#time_log_team_id').val(),
-        subcategoryID = $('#time_log_subcategory_id').val(),
-        date = $('#time_log_date').val(),
-        hours = $('#time_log_hours').val(),
-        minutes = $('#time_log_minutes').val();
+        getValues();
 
         e.preventDefault()
-
-        $.ajax({
-            url:"/time_logs/create",
-            type:"POST",
-            dataType:"HTML",
-            data: {time_log: {
-                              team_id: teamID,
-                              subcategory_id: subcategoryID,
-                              date: date,
-                              hours: hours,
-                              minutes: minutes
-                              }
-            },
-            success: function(response){
-                $('#modal').fadeOut(800);
-                $('#mask').fadeOut(800);
-               $('#team_tables').html(response)
-            }
-        })
-
+       if (canSubmit()) {
+           $.ajax({
+               url: "/time_logs/create",
+               type: "POST",
+               dataType: "HTML",
+               data: {time_log: {
+                   team_id: teamID,
+                   subcategory_id: subcategoryID,
+                   date: date,
+                   hours: hours,
+                   minutes: minutes
+               }
+               },
+               success: function (response) {
+                   $('#modal').fadeOut(800);
+                   $('#mask').fadeOut(800);
+                   $('#team_tables').html(response)
+               }
+           })
+       }
+        else{
+           alert("Please check all fields");
+       }
     })
+
+    var getValues = function(){
+            teamID = $('#time_log_team_id').val(),
+            subcategoryID = $('#time_log_subcategory_id').val(),
+            date = $('#time_log_date').val(),
+            hours = $('#time_log_hours').val(),
+            minutes = $('#time_log_minutes').val();
+    }
+
+    var canSubmit = function(){
+        if (teamID == null ||
+            subcategoryID == null ||
+            date == null ||
+            (hours && minutes == null)){
+             return false
+        }
+        else {
+            return true
+        }
+
+    }
 
 });
